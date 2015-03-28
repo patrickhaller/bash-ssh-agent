9413a89974da550cb761b62ec1915a022e25c7a4
Patrick <phaller@lap4.haller.ws>
2015-03-28 10:13:46 +0800

    add debug

--- ssh.sh
+++ ssh.sh
@@ -4,9 +4,11 @@
 # get list of potential agent sockets
 # is key in list of sockets? use it, return
 # create new agent, add id
+#SSH_DEBUG="1"
+ssh_debug() { [[ $SSH_DEBUG == "1" ]] && echo "$@"; }
 
 ssh_config_get_key_by_host() {
-	local host="$1" host_re="unmatchable" id
+	local host="$1" host_re="unmatchable" id=""
 	local default_id=$(find ${HOME}/.ssh/id_{rsa,dsa} 2>/dev/null | sed 1q)
 	[[ "$host" = "DEFAULT" ]] && { echo $default_id; return 0; }
 	cat ${HOME}/.ssh/config | while read key value; do
@@ -18,20 +20,27 @@ ssh_config_get_key_by_host() {
 ssh_agent_list() {
 	find ${HOME}/.ssh/agent ${HOME}/.ssh-agent* {$TMPDIR,/tmp}/ssh-*/agent* -user $USER -type s 2>/dev/null
 }
-ssh_key_in_list() {
+ssh_find_sock() {
 	local key="$1"
-	export SSH_AUTH_SOCK=$(
-		ssh_agent_list | while read sock; do
-			SSH_AUTH_SOCK=$sock ssh-add -l 2>&1 | grep -qs "$key" && { echo $sock; break; }
-		done
-	)
-	[[ $SSH_AUTH_SOCK != "" ]] && return 0 || return 1
+	ssh_agent_list | while read sock; do
+		SSH_AUTH_SOCK=$sock ssh-add -l 2>&1 | grep -qs "$key" && { echo $sock; break; }
+	done
 }
 ssh_agent_check() {
-	local key=$(ssh_config_get_key_by_host $1 | sed -e "s#~#${HOME}#" )
-	ssh_key_in_list $key && { return 0; }
+	local file=$(ssh_config_get_key_by_host $1 | sed -e "s#~#${HOME}#" )
+	local key="."
+	ssh_debug "agent check got file $file"
+	[[ -f "$file"  ]] && key=$( ssh-keygen -l -f $file | awk '{print $2}' )
+	ssh_debug "looking for id $key"
+	local sock=$( ssh_find_sock $key  )
+	ssh_debug "for this $key got auth sock $sock"
+	if [[ -e $sock ]]; then
+		export SSH_AUTH_SOCK=$sock
+		return 0
+	fi
+	ssh_debug "no agent found, creating new "
 	eval `ssh-agent` &>/dev/null
-	ssh-add $key &>/dev/null
+	ssh-add $file &>/dev/null
 }
 ssh_agent_clean() {
 	local a
